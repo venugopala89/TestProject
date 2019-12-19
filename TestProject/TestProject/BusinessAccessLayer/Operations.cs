63423fc096f1f40db7c7364c8c56966360f4b03b
venugopala89 <venugopala.vtalent@gmail.com>
2019-12-19 17:42:50 +0530

    addition method added

---
+++ TestProject/TestProject/BusinessAccessLayer/Operations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestProject.BusinessAccessLayer
+{
+    public class Operations
+    {
+        public int Addition()
+        {
+            int a=10;
+            int b=20;
+            int c = a + b;
+            return c;
+            
+        }
+
+    }
+}
