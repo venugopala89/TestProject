055353a9e41f0ccf6293be72c861a24d8cc9a1e5
venugopala89 <54429686+venugopala89@users.noreply.github.com>
2019-12-19 17:59:22 +0530

    Update Operations.cs

--- TestProject/TestProject/BusinessAccessLayer/Operations.cs
+++ TestProject/TestProject/BusinessAccessLayer/Operations.cs
@@ -17,6 +17,11 @@ namespace TestProject.BusinessAccessLayer
             return a - b - c;
 
         }
+         public int Multiplication(int a, int b, int c)
+        {
+            return a *b *c;
+
+        }
 
     }
 }
