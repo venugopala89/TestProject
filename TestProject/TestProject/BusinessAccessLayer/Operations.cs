ecec5934f49b3dd755b7c7a3ec0a33d5e3ee1dfd
venugopala89 <venugopala.vtalent@gmail.com>
2019-12-19 17:46:43 +0530

    addition method parameters added

--- TestProject/TestProject/BusinessAccessLayer/Operations.cs
+++ TestProject/TestProject/BusinessAccessLayer/Operations.cs
@@ -7,12 +7,9 @@ namespace TestProject.BusinessAccessLayer
 {
     public class Operations
     {
-        public int Addition()
+        public int Addition(int a,int b)
         {
-            int a=10;
-            int b=20;
-            int c = a + b;
-            return c;
+            return a + b;
             
         }
 
